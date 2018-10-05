3e30bbf5e5e6a8a8e4240a21ea576066b65e94f2
monpolo <39749344+monpolo@users.noreply.github.com>
2018-10-04 19:58:48 -0500

    Update compress.ps1

--- compress.ps1
+++ compress.ps1
@@ -1,2 +1,2 @@
-$base64 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes('C:\Users\monpolo\Desktop\google_RAT-master\stager.ps1'))
+$base64 = [System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes(''))
 $base64
